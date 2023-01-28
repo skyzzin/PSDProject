1f825ff5903571a98cf79c59dcb09578c9e0712c
Douglas Silva Santos <76598304+skyzzin@users.noreply.github.com>
2023-01-27 21:07:46 -0300

    V1

--- PDV/app.py
+++ PDV/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('login.html')
 
+@app.route('/pdv')
+def pdv():
+    return render_template('pdv.html')
+
 if __name__ == '__main__':
     app.run(debug=True)

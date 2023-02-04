13437f55043a6db8e245d8ac5362e791e1206d07
Douglas Silva Santos <76598304+skyzzin@users.noreply.github.com>
2023-02-03 23:56:31 -0300

    V1

--- PDV-Mercado/static/script.js
+++ PDV-Mercado/static/script.js
@@ -27,3 +27,16 @@ function Click(e){
     
 
 }
+
+function write(e){
+    const textArray = e.innerHTML.split('');
+    textArray.forEach((letra,i) => {
+        e.innerHTML='';
+         setTimeout(function(){
+            e.innerHTML += letra
+         },75*i)
+    });
+}
+
+const title = document.querySelector('.title');
+write(title);
